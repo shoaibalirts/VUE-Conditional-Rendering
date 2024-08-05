d3132218fc8ead43f66ada18612892a95a1bab8d
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-05 16:18:25 +0200

    conditional rendering

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      inputGoal: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputGoal);
+      console.log(this.goals);
+      this.inputGoal = "";
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
